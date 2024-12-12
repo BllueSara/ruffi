c1c813ea8517e3200bcc603fc968dcdca3907e20
BluseSara <sara.alluhaibi@outlook.com>
2024-12-12 13:32:24 +0300

    d

---
+++ lib/SplachScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Splachscreen extends StatefulWidget {
+  const Splachscreen({super.key});
+
+  @override
+  State<Splachscreen> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<Splachscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@
 import 'package:flutter/material.dart';
 import 'package:ruffi/splachScreen.dart';
 
+
 void main(){
   runApp(MyApp());
 }
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget{
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SplashScreen(),
+      home: Splachscreen()
     );
   }
 }
